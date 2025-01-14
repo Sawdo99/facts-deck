b44fd476614a1ec7e5717c0d3127f629677e35d6
Sawdo99 <155194856+Sawdo99@users.noreply.github.com>
2025-01-14 12:38:37 -0600

    Part Two Included

--- js_promises/deck_of_cards/index.js
+++ js_promises/deck_of_cards/index.js
@@ -1,5 +1,6 @@
+//Comment out part one or two to see the solutions work. 
 
-
+// PART ONE PROMISES ----------------------------------------------------------------
 // Single card
 fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1')
   .then(response => response.json())
@@ -28,3 +29,42 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
     console.log(`${secondCard.value} of ${secondCard.suit}`);
   })
   .catch(err => console.error('Error fetching cards:', err));
+
+  // PART TWO ASYNC/await -------------------------------------------------------------
+
+  // Single Card
+  async function drawSingleCard() {
+    try {
+      const response = await fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=1');
+      const data = await response.json();
+      const card = data.cards[0];
+      console.log(`${card.value} of ${card.suit}`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  drawSingleCard();
+
+  // Two Cards
+  async function drawTwoCards() {
+    try {
+      const deckResponse = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/');
+      const deckData = await deckResponse.json();
+      const deckId = deckData.deck_id;
+  
+      const firstCardResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+      const firstCardData = await firstCardResponse.json();
+      const firstCard = firstCardData.cards[0];
+      console.log(`${firstCard.value} of ${firstCard.suit}`);
+  
+      const secondCardResponse = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+      const secondCardData = await secondCardResponse.json();
+      const secondCard = secondCardData.cards[0];
+      console.log(`${secondCard.value} of ${secondCard.suit}`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  drawTwoCards();
